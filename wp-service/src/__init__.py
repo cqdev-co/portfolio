05c5fda32a51a8c9c253ebaf746df10ca01551f2
Conor Quinlan <Conor.Quinlan@du.edu>
2025-08-01 15:16:43 -0600

    Adding wp service + CI enhancements

---
+++ wp-service/src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Mathematical Gradient Generator
+
+A Python package for creating beautiful wallpapers using mathematical algorithms
+and color theory.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Portfolio Project"
+__description__ = "Mathematical gradient wallpaper generator"
